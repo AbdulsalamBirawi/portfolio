3c4527277c01ee1e7545d8aa0fd29fdf576520e6
Abdulsalam Birawi <salambirawi99@gmail.com>
2024-07-18 02:18:46 -0700

    commit for angular

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/profile.jpg";
-import CV from "../../assets/abdalsalam_albirawi.pdf";
+import CV from "../../assets/Abdalsalam_Albirawi.pdf";
 import Info from "./Info";
 
 const About = () => {
